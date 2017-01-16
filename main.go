9b31e856ceabcd833abf4a59795dee3ba968c9f7
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-16 15:50:24 +0900

    Print Systemd unit names

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/coreos/go-systemd/dbus"
+)
 
 func main() {
+	conn, err := dbus.New()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	units, err := conn.ListUnits()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	for _, unit := range units {
+		fmt.Println(unit.Name)
+	}
+
 	fmt.Println("ct2stimer")
 }
