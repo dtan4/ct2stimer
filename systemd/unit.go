e8171f6fe2a121009950e7661f6ada33ab215972
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-20 16:54:15 +0900

    Divide systemd package files

---
+++ systemd/unit.go
@@ -0,0 +1,68 @@
+package systemd
+
+import (
+	"bytes"
+	"text/template"
+)
+
+// ServiceData represents data set of systemd Service
+type ServiceData struct {
+	Name    string
+	Command string
+	After   string
+}
+
+// TimerData represents data set of systemd Timer
+type TimerData struct {
+	Name     string
+	Cronspec string
+}
+
+// GenerateService generates new systemd Service
+func GenerateService(name, command, after string) (string, error) {
+	body, err := Asset("templates/service.tmpl")
+	if err != nil {
+		return "", err
+	}
+
+	tmpl, err := template.New("ct2stimer-service").Parse(string(body))
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+
+	if err := tmpl.Execute(&buf, &ServiceData{
+		Name:    name,
+		Command: command,
+		After:   after,
+	}); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
+
+// GenerateTimer generates new systemd Timer
+func GenerateTimer(name, cronspec string) (string, error) {
+	body, err := Asset("templates/timer.tmpl")
+	if err != nil {
+		return "", err
+	}
+
+	tmpl, err := template.New("ct2stimer-timer").Parse(string(body))
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+
+	if err := tmpl.Execute(&buf, &TimerData{
+		Name:     name,
+		Cronspec: cronspec,
+	}); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
